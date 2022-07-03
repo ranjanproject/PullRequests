2184207d6b92bd3bf07c128bb1827dd378487bd2
Pratik Ranjan <pratikranjaniitg101@gmail.com>
2022-07-04 02:08:46 +0530

    diff util added

--- app/src/main/java/com/example/pullrequests/adapters/ClosedPullRequestDiffUtil.kt
+++ app/src/main/java/com/example/pullrequests/adapters/ClosedPullRequestDiffUtil.kt
@@ -1,13 +1,30 @@
 package com.example.pullrequests.adapters
 
 import androidx.recyclerview.widget.DiffUtil
+import com.example.pullrequests.models.PullRequest
 
 class ClosedPullRequestDiffUtil: DiffUtil.ItemCallback<Any>() {
     override fun areItemsTheSame(oldItem: Any, newItem: Any): Boolean {
-        TODO("Not yet implemented")
+        var areItemSame = true
+
+        if(oldItem is PullRequest && newItem is PullRequest){
+            areItemSame = true
+        }
+
+        return areItemSame
     }
 
     override fun areContentsTheSame(oldItem: Any, newItem: Any): Boolean {
-        TODO("Not yet implemented")
+        var areContentSame = true
+
+        if(oldItem is PullRequest && newItem is PullRequest){
+            areContentSame = (oldItem.closedDate == newItem.closedDate) &&
+                    (oldItem.user.userName == newItem.user.userName) &&
+                    (oldItem.user.userImage == newItem.user.userImage)
+                    (oldItem.title == newItem.title) &&
+                    (oldItem.createdDate == newItem.createdDate)
+        }
+
+        return areContentSame
     }
 }
