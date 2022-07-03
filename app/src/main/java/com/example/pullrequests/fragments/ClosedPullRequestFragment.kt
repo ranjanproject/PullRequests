d7c7d1ba049a2020e0a14b7c4acdb51c7194cb98
Pratik Ranjan <pratikranjaniitg101@gmail.com>
2022-07-03 23:32:30 +0530

    Internet permission added

--- app/src/main/java/com/example/pullrequests/fragments/ClosedPullRequestFragment.kt
+++ app/src/main/java/com/example/pullrequests/fragments/ClosedPullRequestFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.navigation.fragment.findNavController
 import com.example.pullrequests.R
@@ -23,7 +25,7 @@ class ClosedPullRequestFragment : Fragment() {
 
 
     private lateinit var binding: FragmentClosedPullRequestBinding
-
+    private val viewModel: ClosedPullRequestViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,7 +42,8 @@ class ClosedPullRequestFragment : Fragment() {
     }
 
     private fun init(){
-
+         initViewModelObservers()
+         viewModel.getClosedPRList(getUserName(), getRepoName())
     }
 
     private fun initRV(){
@@ -48,7 +51,9 @@ class ClosedPullRequestFragment : Fragment() {
     }
 
     private fun initViewModelObservers(){
-
+        viewModel.closedPRListLiveData?.observe(viewLifecycleOwner, Observer {
+            val x = it
+        })
     }
 
     private fun initNetworkState(){
