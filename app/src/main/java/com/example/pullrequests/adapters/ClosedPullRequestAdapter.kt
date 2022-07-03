a1aec7ce6886b5f4c5491344cd54071eb1efc0a9
Pratik Ranjan <pratikranjaniitg101@gmail.com>
2022-07-04 01:55:26 +0530

    Adapter code completed

--- app/src/main/java/com/example/pullrequests/adapters/ClosedPullRequestAdapter.kt
+++ app/src/main/java/com/example/pullrequests/adapters/ClosedPullRequestAdapter.kt
@@ -1,23 +1,45 @@
 package com.example.pullrequests.adapters
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.pullrequests.models.PullRequest
+import com.example.pullrequests.viewHolders.PullRequestItemViewHolder
 
-class ClosedPullRequestAdapter(): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class ClosedPullRequestAdapter(private val context: Context):
+    ListAdapter<Any, RecyclerView.ViewHolder>(ClosedPullRequestDiffUtil()){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+        var viewHolder: RecyclerView.ViewHolder? = null
+        val inflater = LayoutInflater.from(context)
+        when(viewType){
+              PullRequestItemViewHolder.ITEM_PULL_REQUEST ->
+                  viewHolder = PullRequestItemViewHolder.create(inflater, parent)
+        }
+
+        return viewHolder!!
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = getItem(position)
+        when(holder){
+            is PullRequestItemViewHolder -> holder.setDataInView(item as PullRequest)
+        }
     }
 
     override fun getItemViewType(position: Int): Int {
-        return super.getItemViewType(position)
+        super.getItemViewType(position)
+        val item = getItem(position)
+        var viewType = 0
+        when(item){
+            is PullRequest -> viewType = PullRequestItemViewHolder.ITEM_PULL_REQUEST
+        }
+        return viewType
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return itemCount
     }
 }
---
+++ app/src/main/java/com/example/pullrequests/viewHolders/PullRequestItemViewHolder.kt
@@ -0,0 +1,47 @@
+package com.example.pullrequests.viewHolders
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pullrequests.R
+import com.example.pullrequests.databinding.ItemPullRequestBinding
+import com.example.pullrequests.models.PullRequest
+
+class PullRequestItemViewHolder(private val binding: ItemPullRequestBinding): RecyclerView.ViewHolder(binding.root) {
+    companion object{
+        const val ITEM_PULL_REQUEST = R.layout.item_pull_request
+        fun create(inflater: LayoutInflater,
+                   parent: ViewGroup
+        ): PullRequestItemViewHolder{
+            val binding = ItemPullRequestBinding.inflate(inflater, parent, false)
+            return PullRequestItemViewHolder(binding)
+        }
+    }
+
+    fun setDataInView(pullRequest: PullRequest){
+        setUserName(pullRequest.user.userName)
+        setUserImage(pullRequest.user.userImage)
+        setTitle(pullRequest.title)
+        setCreatedDate(pullRequest.createdDate)
+        setClosedDate(pullRequest.closedDate)
+    }
+
+    private fun setUserName(userName: String){
+    }
+
+    private fun setUserImage(userImage: String){
+
+    }
+
+    private fun setTitle(title: String){
+
+    }
+
+    private fun setCreatedDate(createdDate: String){
+
+    }
+
+    private fun setClosedDate(closedDate: String){
+
+    }
+}
